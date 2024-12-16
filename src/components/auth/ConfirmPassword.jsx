364381f9787104c7f6d62b78c4f94f05e7f1df6f
Dharmateja69 <ddharmateja69@gmail.com>
2024-12-16 15:26:55 +0530

    done with confirm password

--- src/components/auth/ConfirmPassword.jsx
+++ src/components/auth/ConfirmPassword.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Container from "../Container";
+import Forminput from "../form/Forminput";
+import Submit from "../form/Submit";
+import Title from "../form/Title";
 
 export default function ConfirmPassword() {
   return (
-    <div>
-      ConfirmPassword
+    <div className="fixed bg-primary inset-0 -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 w-96 space-y-6">
+          <Title>Enter new Pasword</Title>
+          <Forminput label="New Password" placeholder="*******" name="password" type="password" />
+          <Forminput label="Confirm Password" placeholder="*******" name="ConfirmPassword" type="password"/>
+
+          <Submit value="Confirm Password" />
+        </form>
+      </Container>
     </div>
-  )
+  );
 }
